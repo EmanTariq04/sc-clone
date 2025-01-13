07ec2153f1e0df6f2e7cedef90d58e32c127f152
Eman Tariq <emantariq.1004@gmail.com>
2025-01-13 22:40:41 +0500

    another day another commit

--- src/Chat.js
+++ src/Chat.js
@@ -1,10 +1,27 @@
-import React from 'react'
-import './Chat.css'
+import React from "react";
+import "./Chat.css";
+import { Avatar } from "@mui/material";
+import StopRoundedIcon from "@mui/icons-material/StopRounded";
+import ReactTimeago from "react-time-ago";
 
-function Chat() {
+function Chat({ id, username, timestamp, read, imageUrl, profilePic }) {
   return (
-    <div>Chat</div>
-  )
+    <div className="chat">
+      <Avatar className="chat-avatar" src={profilePic} />
+      <div className="chat-info">
+        <h4>{username}</h4>
+        <p>
+          Tap to view -{" "}
+          {timestamp && timestamp.toDate ? (
+            <ReactTimeago date={timestamp.toDate()} />
+          ) : (
+            "No Date Available"
+          )}
+        </p>
+      </div>
+      {!read && <StopRoundedIcon className="chat-readicon" />}
+    </div>
+  );
 }
 
-export default Chat
+export default Chat;
