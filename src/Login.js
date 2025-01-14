ca73f0739effc2faf1ff5f622a8bbe947ff6761b
Eman Tariq <emantariq.1004@gmail.com>
2025-01-14 16:31:01 +0500

    another commit

--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,21 @@
-import React from 'react'
-import "./Login.css"
+import React from "react";
+import "./Login.css";
+import { Button } from "@mui/material";
 
 function Login() {
+  const signIn = () => {};
+
   return (
-    <div>Login</div>
-  )
+    <div className="login">
+      <div className="login-container">
+        <img
+          src="https://www.usatoday.com/gcdn/presto/2019/08/16/USAT/bd6538e4-5535-41ce-857b-518451c3a958-Snapchat_Logo_H.png?crop=1125,1500,x674,y0"
+          alt=""
+        />
+        <Button variant="outlined" onClick={signIn}>Sign in</Button>
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
